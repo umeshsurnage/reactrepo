d548ffb8d39b153b36abc9a7eaf888972dce56eb
UMESH SURANGE <138711278+umeshsurnage@users.noreply.github.com>
2024-02-07 22:27:16 +0530

    js to jsx

---
+++ projects/src/App.jsx
@@ -0,0 +1,65 @@
+import logo from "./logo.svg"
+import "./App.css"
+import UseEffect from "./compo/UseEffect1/UseEffect1"
+
+// import { Header2 } from "./compo/Header2"
+// import UserList from "./compo/UserList"
+
+// 2) FbPost
+// import FbPost from "./compo/FbPost/FbPost"
+// function App() {
+//     return <FbPost />
+// }
+
+// 3) Counter ( Add row and Delete all row's)
+// import Counter from "./compo/Counter/Counter"
+// function App(){
+//   return <Counter></Counter>
+// }
+
+//4 ) Table / crud
+// import Table from "./Table/Table"
+// function App(){
+//   return <Table></Table>
+// }
+
+//5) To do
+// import TodoDemo from "./compo/TodoList/TodoDemo"
+
+// function App(){
+//   return <TodoDemo></TodoDemo>
+// }
+
+// 6) tic tac toe
+// import TicTacToe from "./compo/TicTacToe/TicTacToe"
+// function App(){
+//   return <TicTacToe></TicTacToe>
+// }
+
+ // 7) form
+//  import Forms from "./compo/Forms/Forms"
+//  function App(){
+//   return <Forms></Forms>
+//  }
+
+// 8) Carausel
+// import Carausel from "./compo/Carausel/Carausel"
+// function App(){
+//   return <>
+//   <Carausel></Carausel>
+//   </>
+// }
+
+//9) useEffect counter demo 1
+  // function App(){ 
+  //   return <>
+  //     <UseEffect></UseEffect>
+  //   </>
+  // }
+
+// 10) Slider using useEffect
+  import Slider from "./compo/Slider/Slider"
+  function App(){
+    return <Slider></Slider>
+  }
+export default App
